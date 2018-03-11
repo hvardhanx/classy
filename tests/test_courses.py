21bd66a9a45065188dfd8bfbf60298cff106fc94
Harsh Vardhan <harshvd95@gmail.com>
2018-03-11 18:04:38 +0530

    Fix test

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -8,7 +8,6 @@ User = get_user_model()
 class TestCoursesApi(TestCase):
     def setUp(self):
         user = User.objects.create(username='test', email='test@test.com')
-        user.save()
         subject = Subject.objects.create(title='Computer Science', slug='computer-science')
         course = Course.objects.create(owner=user, subject=subject, title='Computer Networks', slug='computer-network',
                                        overview='Learn about networking')
